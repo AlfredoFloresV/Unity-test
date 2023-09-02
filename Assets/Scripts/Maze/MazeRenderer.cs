8a7325b61768c948fd8de8e3fc81d7c0ab244664
Sergio Damian <sergio.damian181091@gmail.com>
2023-09-01 20:13:54 -0600

    maze v1 and player controller-pc

---
+++ Assets/Scripts/Maze/MazeRenderer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRenderer : MonoBehaviour
+{
+    [SerializeField] MazeGenerator mazeGenerator;
+    [SerializeField] GameObject mazeCellPrefab;
+
+    public float CellSize = 1f;
+
+    private void Start()
+    {
+        MazeCell[,] maze = mazeGenerator.GetMaze();
+
+        for (int x = 0; x < mazeGenerator.mazeWidth; x++) {
+            for (int y = 0; y < mazeGenerator.mazeHeight; y++) { 
+                GameObject newCell = Instantiate(mazeCellPrefab, 
+                                                 new Vector3((float)x * CellSize, 0f, (float)y*CellSize),
+                                                 Quaternion.identity,
+                                                 transform);
+
+                newCell.transform.localScale = new Vector3( newCell.transform.localScale.x * CellSize,
+                                                            newCell.transform.localScale.y * CellSize,
+                                                            newCell.transform.localScale.z * CellSize
+                                                          );
+
+                MazeCellObject mazeCell = newCell.GetComponent<MazeCellObject>();
+
+                bool top = maze[x, y].topWall;
+                bool left = maze[x, y].leftWall;
+
+                bool right = false;
+                bool bottom = false;
+
+
+                if (x == mazeGenerator.mazeWidth - 1) right = true;
+                if (y == 0) bottom = true;
+
+                mazeCell.init(top, bottom, right, left);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float sensitivityX = 15f;
+    [SerializeField]
+    private float sensitivityY = 5f;
+
+    private PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMove;
+        Vector3 movVertical = transform.forward * zMove;
+
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+        motor.Move(velocity);
+
+        // Rotation
+        float yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * sensitivityX;
+        motor.Rotate(rotation);
+
+        // Camera Rotation
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 cameraRotation = new Vector3(xRot, 0f, 0f) * sensitivityY;
+        motor.RotateCamera(cameraRotation);
+    }
+}
