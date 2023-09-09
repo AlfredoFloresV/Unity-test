4a97aed958a729fb38c4e27c3c4876e6b57673da
AlfredoFloresV <58006866+AlfredoFloresV@users.noreply.github.com>
2023-09-08 23:42:42 -0600

    ADD Funhouse
    ADD Funhouse
    ADD Terrain assets
    ADD Spin script

---
+++ Assets/Scripts/Other/SpinObject.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SpinObject : MonoBehaviour
+{
+    public GameObject pivotPoint; // The GameObject to rotate around
+    public float spinSpeed = 60.0f; // Speed of rotation in degrees per second
+    public bool reverseSpin = false; // Set to true to reverse the spin direction
+
+    void Update()
+    {
+        if (pivotPoint != null)
+        {
+            // Calculate the rotation axis relative to the pivot point
+            Vector3 pivotToGameObject = transform.position - pivotPoint.transform.position;
+            Vector3 rotationAxis = transform.right; // Use local right axis for X-axis rotation
+
+            // Determine the rotation direction based on the reverseSpin boolean
+            float direction = reverseSpin ? -1.0f : 1.0f;
+
+            // Rotate the GameObject around the pivot point with the specified direction
+            transform.RotateAround(pivotPoint.transform.position, rotationAxis, direction * spinSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.LogWarning("Pivot point not assigned. Please assign a GameObject to the 'Pivot Point' field in the Inspector.");
+        }
+    }
+}
