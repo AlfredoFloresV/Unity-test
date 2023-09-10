57ab8b2e13c17eea57b2dc548f99cee513d70251
Iván <loboestepario91@gmail.com>
2023-09-09 20:53:29 -0600

    Update Pause Controller. Now it pauses and resume
    The tittle, igual Alfredo ni sabe leer.

--- Assets/Scripts/Menus/PauseMenuController.cs
+++ Assets/Scripts/Menus/PauseMenuController.cs
@@ -9,9 +9,30 @@ public class PauseMenuController : MonoBehaviour
     [Header("Levels to Load")]
     public string _mainMenu;
     private string levelToLoad;
-    public void ResumePlay()
+
+    static public bool gameIsPaused;
+
+    public GameObject pauseMenuUI;
+
+    void Update(){
+        if ( Input.GetKeyDown(KeyCode.Escape)){
+            if( gameIsPaused ){ 
+                this.ResumePlay();
+            }else{
+                this.Pause();
+            }
+        }
+    }
+    void ResumePlay()
     {
-        Debug.Log("Todo Resume");
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
     }
     public void ExitGameDialog()
     {
